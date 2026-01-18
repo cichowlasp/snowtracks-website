7863ad1539d5788ed80c30294f4ac78eaba424d1
Piotr Cichowlas <cichowlas.p@gmail.com>
2026-01-18 13:04:10 +0100

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://snowtracks.cichowlasp.org',
+  site: 'https://snowtracks.cichowlasp.org',
 
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
+
+  integrations: [sitemap()],
 });
