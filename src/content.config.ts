76ec59028d5ecad31d406b0119ec9e1c3490d884
Piotr Cichowlas <cichowlas.p@gmail.com>
2026-04-23 13:08:23 +0200

    updated packages and images

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,4 @@
-// src/content/config.ts
+// src/content.config.ts
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
@@ -10,3 +10,4 @@ const legal = defineCollection({
 });
 
 export const collections = { legal };
+
